d1c4826b8f879330a2a9f56716758c98208d83de
Cam <84293029+camillaxyc@users.noreply.github.com>
2024-12-10 17:59:21 -0800

    final project

---
+++ virtual_cat.js
@@ -0,0 +1,120 @@
+class Cat {
+  constructor(name, imageUrl) {
+    this.imageUrl = imageUrl;
+    this.name = name;
+  }
+  meow() {
+    return `${this.name} meows`;
+  }
+}
+
+const formEl = document.getElementById("userForm");
+const userNameEl = document.getElementById("userName");
+const numofCatEl = document.getElementById("numofCat");
+const main = document.getElementsByTagName("main")[0];
+const intro = document.getElementById("intro");
+const adopting = document.getElementById("adopting");
+const yNbuttonGroup = document.getElementById("yNbuttonGroup");
+const yesButton = document.getElementById("yesButton");
+const noButton = document.getElementById("noButton");
+const end = document.getElementById("end");
+const userInfo = document.getElementById("userInfo");
+const catInfo = document.getElementById("catInfo");
+const noCat = document.getElementById("noCat");
+const feedCat = document.getElementById("feedCat");
+
+end.classList.add("hidden");
+adopting.classList.add("hidden");
+feedCat.classList.add("hidden");
+const catArray = [];
+console.log(main);
+
+feedCat.addEventListener("click", () => {
+  alert("Cat is fed");
+});
+
+console.log("testing");
+
+const validInputLength = (input, min) => {
+  if (input.value.trim().length >= min) {
+    input.parentElement.classList.remove("invalid");
+    return true;
+  } else {
+    input.parentElement.classList.add("invalid");
+    return false;
+  }
+};
+
+const adoptCat = (Cat) => {
+  catArray.push(Cat);
+  console.log("cat array is " + catArray);
+  console.log(catArray);
+};
+
+formEl.addEventListener("submit", function (e) {
+  e.preventDefault();
+  if (
+    validInputLength(userNameEl, 3) &&
+    numofCatEl.value > 0 &&
+    numofCatEl.value < 10
+  ) {
+    intro.classList.add("hidden");
+    adopting.classList.remove("hidden");
+    const userName = userNameEl.value;
+    const numofCat = numofCatEl.value;
+    const BASE_URL = "https://api.thecatapi.com/v1/images/search";
+    const url = `${BASE_URL}?api-key=${myKey}`;
+    console.log(url);
+
+    async function fetchCatData(url) {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+
+        console.log("Data fetched:", data);
+        showCat(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+
+    function showCat(data) {
+      const catImage = document.createElement("img");
+      catImage.style.width = "300px";
+      //   adopting.appendChild(catImage);
+      adopting.insertBefore(catImage, adopting.firstChild);
+      const catImageUrl = JSON.stringify(data[0].url);
+      const catImageSrc = catImageUrl.substring(1, catImageUrl.length - 1);
+      catImage.src = catImageSrc;
+      const adoptingCat = new Cat(catName.value, catImageSrc);
+      //   adoptCat(adoptingCat);
+      yesButton.addEventListener("click", () => {
+        e.preventDefault();
+        adoptCat(adoptingCat);
+        adopting.classList.add("hidden");
+        end.classList.remove("hidden");
+        userInfo.innerHTML = `Congrats ${userName}! You've adopted ${catArray[0].name}`;
+        catInfo.appendChild(catImage);
+        setTimeout(function () {
+          feedCat.classList.remove("hidden");
+        }, 2000);
+      });
+    }
+    noButton.addEventListener("click", () => {
+      adopting.classList.add("hidden");
+      noCat.innerHTML = `You did not adopt any cats :(`;
+    });
+    fetchCatData(url);
+  }
+});
+
+// One or more Classes (must use static methods and/or prototype methods)
+// Write testable code, use Jasmine unit tests
+// One or more timing functions
+// One or more fetch requests to a 3rd party API
+// Sets, updates, or changes local storage
+// Contains form fields, validates those fields
